67c698f6bee32dda6dd8f50faf9b44242c049e51
Marina Khai <62333875+MarinaKhai@users.noreply.github.com>
2021-05-31 19:55:01 +0500

    Add files via upload
    Added 2 more buttons to walk faster across numbers. 
    Changed layout to grid one, and the app still needs some improvement in style

--- simpleCounter/main.js
+++ simpleCounter/main.js
@@ -1,5 +1,7 @@
 let arrowUp = document.querySelector('.up');
+let arrowTenUp = document.querySelector('.tenUp');
 let arrowDown = document.querySelector('.down');
+let arrowTenDown = document.querySelector('.tenDown');
 let nmb = document.querySelector('.number');
 
 arrowUp.addEventListener('click', () => {
@@ -8,4 +10,14 @@ arrowUp.addEventListener('click', () => {
 
 arrowDown.addEventListener('click', () => {
     --nmb.innerHTML;
+});
+
+arrowTenUp.addEventListener('click', () => {
+    let tenUp = Number(nmb.innerHTML);
+    nmb.innerHTML = tenUp + 10;
+});
+
+arrowTenDown.addEventListener('click', () => {
+    let tenDown = Number(nmb.innerHTML);
+    nmb.innerHTML = tenDown - 10;
 });
