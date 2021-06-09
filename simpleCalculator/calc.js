884fc358fb51bcf09232a230036cad29e4658dc3
MarinaSh-Kh <marinpostbox@gmail.com>
2021-06-09 22:44:11 +0500

    added some more weirdness in calc's functionality

--- simpleCalculator/calc.js
+++ simpleCalculator/calc.js
@@ -1,34 +1,38 @@
 'use strict'
 
 // var field = document.getElementById('window');
-var text = document.getElementById('text-field');
+var mainLine = document.getElementById('main-field');
+var secondLine = document.getElementById('second-field');
 var digits = document.getElementsByClassName('digit');
 
 var plus = document.getElementById('plus');
 var minus = document.getElementById('minus');
 var mult = document.getElementById('mult');
 var divide = document.getElementById('divide');
-
 var enter = document.getElementById('enter');
 var clear = document.getElementById('clear');
 
 
 for (let i of digits) {
     i.onclick = function () {
-        text.textContent += this.textContent;
+        mainLine.textContent += this.textContent;
     }
 }
 
-let nmb = Number(text.textContent);
+// secondLine.textContent = mainLine.textContent;
+var nmb = Number(secondLine.textContent);
 
 plus.onclick = function() {
-    nmb += Number(text.textContent);
-    text.textContent = '';
-    
+    nmb += Number(mainLine.textContent);
+    secondLine.textContent += mainLine.textContent + ' + ';
+    mainLine.textContent = '';
+    // console.log(n);
 }
+
 enter.onclick = function() {
-    console.log(nmb);
-    text.textContent = '';
+    nmb += Number(mainLine.textContent);
+    secondLine.textContent += mainLine.textContent;
+    mainLine.textContent = nmb;
 }       
 
 // enter.onclick = function() {
@@ -36,6 +40,8 @@ enter.onclick = function() {
 // }
 
 clear.onclick = function () {
-    text.textContent = '';
+    mainLine.textContent = '';
+    secondLine.textContent = '';
+    nmb = 0;
 }
 
