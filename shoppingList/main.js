7a1702e8a2a1d8de56ae1c77d0c251e7b50cc28a
MarinaSh-Kh <marinpostbox@gmail.com>
2021-06-17 20:05:26 +0500

    fixed styles of li buttons and some other elements, added media queries

--- shoppingList/main.js
+++ shoppingList/main.js
@@ -1,6 +1,6 @@
 var myList = document.querySelector('ol');
-const inputField = document.querySelector('input');
-const addBtn = document.querySelector('.addBtn');
+const inputField = document.querySelector('.add_field');
+const addBtn = document.querySelector('.add_btn');
 const clearBtn = document.querySelector('.invisible');
 
 addBtn.onclick = function() {
@@ -12,16 +12,20 @@ addBtn.onclick = function() {
     const deleteBtn = document.createElement('button');
     const doneBtn = document.createElement('button');
     
-    deleteBtn.setAttribute('class', 'delete-btn');
-    deleteBtn.textContent = 'Delete';
-    doneBtn.setAttribute('class', 'done-btn');
-    doneBtn.textContent = 'Done';
+    deleteBtn.setAttribute('class', 'delete_btn');
+    deleteBtn.innerHTML = 'Delete';
+    doneBtn.setAttribute('class', 'done_btn');
+    doneBtn.innerHTML = 'Done';
+    
+    if (window.innerWidth < 640) {
+        doneBtn.innerHTML = '&#10003;';
+        deleteBtn.innerHTML = '	&#10006;';
+    }
 
     listItem.appendChild(itemText);
     listItem.appendChild(deleteBtn);
     listItem.appendChild(doneBtn);
     listItem.setAttribute('style', 'text-decoration: none');
-
 
     itemText.textContent = inputFieldValue + ' ';
     myList.appendChild(listItem);
@@ -38,16 +42,17 @@ addBtn.onclick = function() {
     }
     doneBtn.onclick = function() {
         if (listItem.style.textDecoration === 'none') {
-            listItem.style.textDecoration = 'line-through';
+            listItem.style = 'text-decoration: line-through; color: #bbb';
             doneBtn.style.backgroundColor = '#eee';
         }
         else {
-            listItem.style.textDecoration = 'none';
+            listItem.style = 'text-decoration: none; color: #000';
             doneBtn.style.backgroundColor = '#fff';
         }
-
+    }
 }
 
+// Add items by pressing enter when cursor is in input field
 inputField.onkeydown = function(e) {
     if (e.keyCode === 13) {
         let inputFieldValue = inputField.value;
@@ -58,10 +63,15 @@ inputField.onkeydown = function(e) {
         const deleteBtn = document.createElement('button');
         const doneBtn = document.createElement('button');
         
-        deleteBtn.setAttribute('class', 'delete-btn');
-        deleteBtn.textContent = 'Delete';
-        doneBtn.setAttribute('class', 'done-btn');
-        doneBtn.textContent = 'Done';
+        deleteBtn.setAttribute('class', 'delete_btn');
+        deleteBtn.innerHTML = 'Delete';
+        doneBtn.setAttribute('class', 'done_btn');
+        doneBtn.innerHTML = 'Done';
+        
+        if (window.innerWidth < 640) {
+            doneBtn.innerHTML = '&#10003;';
+            deleteBtn.innerHTML = '	&#10006;';
+        }
 
         listItem.appendChild(itemText);
         listItem.appendChild(deleteBtn);
@@ -83,13 +93,16 @@ inputField.onkeydown = function(e) {
         }
         doneBtn.onclick = function() {
             if (listItem.style.textDecoration === 'none') {
-                listItem.style.textDecoration = 'line-through';
+                listItem.style = 'text-decoration: line-through; color: #bbb';
                 doneBtn.style.backgroundColor = '#eee';
             }
             else {
-                listItem.style.textDecoration = 'none';
+                listItem.style = 'text-decoration: none; color: #000';
                 doneBtn.style.backgroundColor = '#fff';
             }
         }
     }
 }
+// To do:
+// перенос текста в ли на новую строку, чтобы не ломалась разметка у кнопок
+// динамическое изменение содержимого кнопок ли при иизменении размера экрана
